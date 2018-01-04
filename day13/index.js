5d71b179365b80152d658c2d5f0fac0c1fa1b918
hank <henrik@hankolsen.com>
2018-01-05 00:50:02 +0100

    Refactored day 13

--- day13/index.js
+++ day13/index.js
@@ -8,36 +8,34 @@ getRows()
     const happinessMap = data
       .map((row) => {
         const [, person, direction, value, target] = row.match(/(\w+).*(lose|gain) (\d+).* (\w+)\./);
-        return { person, target, value: direction === 'gain' ? +value : -value };
+        return [person, target, direction === 'gain' ? +value : -value];
       })
-      .reduce((map, obj) => {
-        map[obj.person] = { ...map[obj.person], [obj.target]: obj.value };
+      .reduce((map, [person, target, value]) => {
+        map[person] = { ...map[person], [target]: value };
         return map;
       }, {});
-
 
     const nextPerson = (row, index) => row[(index + 1) % row.length];
     const nextPersonWithMe = (row, index) => index + 1 === row.length ? 'Me' : nextPerson(row, index);
     const coupleHappiness = (a, b) => b === 'Me' ? 0 : happinessMap[a][b] + happinessMap[b][a];
 
-    const part1 = () => {
+
+    const findMaxHappiness = (nextPersonFunction) => {
       let max = -1;
       permutator(Object.keys(happinessMap)).forEach((row) => {
-        const result = row.reduce((sum, person, index) => sum + coupleHappiness(person, nextPerson(row, index)), 0);
+        const result = row.reduce((sum, person, index) => sum + coupleHappiness(person, nextPersonFunction(row, index)), 0);
         max = Math.max(max, result);
       });
-      console.log(max);
+      return max;
+    };
+
+    const part1 = () => {
+      console.log(findMaxHappiness(nextPerson));
     };
 
     const part2 = () => {
-      let max = -1;
-      permutator(Object.keys(happinessMap)).forEach((row) => {
-        const result = row.reduce((sum, person, index) => sum + coupleHappiness(person, nextPersonWithMe(row, index)), 0);
-        max = Math.max(max, result);
-      });
-      console.log(max);
+      console.log(findMaxHappiness(nextPersonWithMe));
     };
-
 
     part1();
     part2();
