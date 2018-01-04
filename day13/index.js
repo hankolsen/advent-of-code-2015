9e061541c4584313ec513f4d749618b7d9523575
hank <henrik@hankolsen.com>
2018-01-05 00:32:33 +0100

    Refactored day 13 part 1

--- day13/index.js
+++ day13/index.js
@@ -5,7 +5,7 @@ const { getRows, permutator } = require('../utils');
 getRows()
   .then((data) => {
 
-    const input = data
+    const hapinessMap = data
       .map((row) => {
         const [, person, direction, value, target] = row.match(/(\w+).*(lose|gain) (\d+).* (\w+)\./);
         return { person, target, value: direction === 'gain' ? +value : -value };
@@ -15,10 +15,14 @@ getRows()
         return map;
       }, {});
 
+
+    const nextPerson = (row, index) => row[(index + 1) % row.length];
+    const coupleHappiness = (a, b) => hapinessMap[a][b] + hapinessMap[b][a];
+
     const part1 = () => {
       let max = -1;
-      permutator(Object.keys(input)).forEach((row) => {
-        const result = row.reduce((sum, person, index) => sum + input[person][row[(index + 1) % row.length]] + input[row[(index + 1) % row.length]][person], 0);
+      permutator(Object.keys(hapinessMap)).forEach((row) => {
+        const result = row.reduce((sum, person, index) => sum + coupleHappiness(person, nextPerson(row, index)), 0);
         max = Math.max(max, result);
       });
       console.log(max);
