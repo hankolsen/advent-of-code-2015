1995ee4852da05cff2d87e45234535e9a182b923
hank <henrik@hankolsen.com>
2018-01-04 22:06:54 +0100

    Added day 10 part 2 with refactoring

--- day10/index.js
+++ day10/index.js
@@ -5,23 +5,15 @@ const { getRows } = require('../utils');
 getRows()
   .then((data) => {
 
-    let input = data[0];
+    const input = data[0];
 
-    const count = (str) => str.match(/(.)\1*/g).reduce((result, chars) => result + chars.length + chars[0], '');
+    const lookAndSay = str => str.match(/(.)\1*/g).reduce((result, chars) => result + chars.length + chars[0], '');
+    const repeat = (turns, string) => [...Array(turns)].reduce(lookAndSay, string);
 
-    const part1 = () => {
-      const numberOfTurns = 40;
-      for (let i = 0; i < numberOfTurns; i += 1) {
-        input = count(input);
-      }
-      console.log(input.length);
-    };
+    const result1 = repeat(40, input);
+    const result2 = repeat(10, result1);
 
-    const part2 = () => {
-    };
-
-    part1();
-    part2();
+    console.log(result1.length, result2.length);
 
   })
   .catch(err => console.log(`There was an error\n${err}`));
