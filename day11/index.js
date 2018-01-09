e0420669134bf9422e2453f96e0f5d6eb81f9d54
hank <henrik@hankolsen.com>
2018-01-09 01:33:24 +0100

    Added day 11 part 1

--- day11/index.js
+++ day11/index.js
@@ -10,9 +10,34 @@ getRow()
     const testDoubles = string => string.match(/(.)\1.*(.)\2/);
     const testForbiddenCharacters = string => !(string.match(/[iol]/) || []).length;
     const isValidPassword = password => [test3Consecutive, testDoubles, testForbiddenCharacters].every(test => test(password));
+    const findNewPassword = (password, position = password.length - 1) => {
+      let charCode = password.charCodeAt(position);
+      if (charCode === 122) {
+        password = password.substr(0, position) + 'a' + password.substr(position + 1);
+        return findNewPassword(password, position - 1);
+      }
+      charCode += 1;
+
+      // i, l, o are not allowed
+      if (charCode === 105 || charCode === 108 || charCode === 111) {
+        charCode += 1;
+      }
+
+      password = password.substr(0, position) + String.fromCharCode(charCode) + password.substr(position + 1);
+      return password;
+    };
 
 
-    console.log(isValidPassword(input));
+    const part1 = () => {
+      let password = input;
+      while (!isValidPassword(password)) {
+        password = findNewPassword(password);
+      }
+
+      console.log(password);
+    };
+
+    part1();
 
   })
   .catch(err => console.log(`There was an error\n${err}`));
